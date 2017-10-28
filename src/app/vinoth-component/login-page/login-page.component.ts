bfbf75b6201226c91acd89d98243e3105a8c2b5e
vinoth214 <vinu.vinotheee@gmail.com>
2017-10-28 13:09:35 +0530

    facebook login integration added

--- src/app/vinoth-component/login-page/login-page.component.ts
+++ src/app/vinoth-component/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+  import { Component, OnInit } from '@angular/core';
+import { FacebookService, LoginResponse, LoginOptions, UIResponse, UIParams, FBVideoComponent,InitParams } from 'ngx-facebook';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FacebookService,public router: Router) {
+    console.log('Initializing Facebook');
+
+    fb.init({
+      appId: '1927971220769787',
+      version: 'v2.9'
+    });
+
+
+ 
+  //   fb.init(initParams);
+  //     this.fb.api('somepath')
+  // .then(res => console.log(res))
+  // .catch(e => console.log(e));
+  
+
+   }
 
   ngOnInit() {
   }
+
+
+// login() {
+//     this.fb.login()
+//       .then((res: LoginResponse) => {
+//         this.router.navigate(['dashBoard']);
+//         console.log('Logged in', res);
+
+//       })
+//       .catch(this.handleError);
+//   }
+
+
+
+// loginWithOptions() {
+    login() {
+    const loginOptions: LoginOptions = {
+      enable_profile_selector: true,
+      return_scopes: true,
+      scope: 'public_profile,user_friends,email,pages_show_list'
+    };
+
+    this.fb.login(loginOptions)
+      .then((res: LoginResponse) => {
+        this.router.navigate(['dashBoard']);
+        console.log('Logged in', res);
+      })
+      .catch(this.handleError);
+
+  }
+
+
+  private handleError(error) {
+    console.error('Error processing action', error);
+  }
+  
 
 }
