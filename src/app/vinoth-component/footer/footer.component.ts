d1cded48e3da800ae1c221fb9b6b0a68e7cc7466
vinoth214 <vinu.vinotheee@gmail.com>
2017-12-16 00:31:29 +0530

    Dashboard page added

--- src/app/vinoth-component/footer/footer.component.ts
+++ src/app/vinoth-component/footer/footer.component.ts
@@ -5,11 +5,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
+ 
 export class FooterComponent implements OnInit {
-
+  mySidebar:any
+  overlayBg:any
   constructor() { }
 
   ngOnInit() {
+// Get the Sidebar
+    this.mySidebar = document.getElementById("mySidebar");
+    
+    // Get the DIV with overlay effect
+     this.overlayBg = document.getElementById("myOverlay");
   }
+
+  
+  
+
+  
+  // Toggle between showing and hiding the sidebar, and add overlay effect
+   w3_open() {
+      if (this.mySidebar.style.display === 'block') {
+          this.mySidebar.style.display = 'none';
+          this.overlayBg.style.display = "none";
+      } else {
+          this.mySidebar.style.display = 'block';
+          this.overlayBg.style.display = "block";
+      }
+  }
+  
+  // Close the sidebar with the close button
+   w3_close() {
+      this.mySidebar.style.display = "none";
+      this.overlayBg.style.display = "none";
+  }
+
 
 }
